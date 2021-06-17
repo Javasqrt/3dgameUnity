ba0011c7a067c461bdc5c5ff76a9ea468e96ec24
Anton <t.patap99@gmail.com>
2021-06-17 16:34:28 +0300

    version 3

--- Assets/Scripts/SizeOfSphere.cs
+++ Assets/Scripts/SizeOfSphere.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SizeOfSphere : MonoBehaviour
 {
     [SerializeField] private Bullet bullet;
+    [SerializeField] private GameObject lose_canvas;
     private Transform _gameObject;
-    private  int _minChangeSize = 1;
-    private  int _maxChangeSize = 8;
-    private  int _step = 1;
-    private static int _currentScaleSize;
+    private  float _minChangeSize = 1f;
+    private  float _maxChangeSize = 8f;
+    private  float _step = 1f;
+    private static float _currentScaleSize;
 
 
 
@@ -21,45 +24,41 @@ public class SizeOfSphere : MonoBehaviour
         _gameObject = GetComponent<Transform>();
 
 
-        _currentScaleSize = 0;
+        _currentScaleSize = _minChangeSize;
+  
     }
 
     
    private void Update()
     {
-       
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.touchCount > 0)
         {
-
-
-
-
-           
+            if (Input.GetTouch(0).phase == TouchPhase.Stationary)
+            {
                 if (_currentScaleSize <= _maxChangeSize)
                 {
-                    _currentScaleSize += _step;
+                    _currentScaleSize += _step * Time.deltaTime;
+
+                }
+
+
+               
 
             }
-
-
-            bullet.DeleteOldGameObject();
-            
+           
         }
-       
 
         if (Input.GetMouseButtonUp(0))
         {
+            bullet.DeleteOldGameObject();
             Debug.Log(_currentScaleSize);
             bullet.InitBulletWithSize(_currentScaleSize);
             _gameObject.transform.localScale = new Vector3(_gameObject.transform.localScale.x - (_gameObject.transform.localScale.x * (_currentScaleSize / 10f)), _gameObject.transform.localScale.y - (_gameObject.transform.localScale.y * (_currentScaleSize / 10f)), _gameObject.transform.localScale.z - (_gameObject.transform.localScale.z * (_currentScaleSize / 10f)));
 
 
-
-
-
         }
-
+      
 
     }
 
